fac7cb3b4d09c25aa038ad82b636fdcf346b32b2
joel.weon <joel.weon@gmail.com>
2017-01-04 19:58:44 +0900

    .

--- src/main/java/bitcamp/java89/ems2/control/project/ProjectUpdateControl.java
+++ src/main/java/bitcamp/java89/ems2/control/project/ProjectUpdateControl.java
@@ -6,19 +6,19 @@ import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+
+import bitcamp.java89.ems2.control.PageController;
 import bitcamp.java89.ems2.dao.ProjectDao;
 import bitcamp.java89.ems2.domain.Project;
 import bitcamp.java89.ems2.listener.ContextLoaderListener;
 import bitcamp.java89.ems2.util.MultipartUtil;
 
-@WebServlet("/project/update")
-public class ProjectUpdateControl extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+@Controller("/project/update.do")
+public class ProjectUpdateControl implements PageController {
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) 
@@ -114,5 +114,11 @@ public class ProjectUpdateControl extends HttpServlet {
       rd.forward(request, response);
       return;
     }
+  }
+
+  @Override
+  public String service(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    // TODO Auto-generated method stub
+    return null;
   }
 }
